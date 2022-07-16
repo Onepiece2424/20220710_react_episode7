e59a1f4a93ad081a065298fbcc0b3e77050be425
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2022-07-17 06:40:04 +0900

    Contextの値からEditButton.jsxから参照

--- src/components/EditButton.jsx
+++ src/components/EditButton.jsx
@@ -1,3 +1,9 @@
+//"react"からuseContextをimport
+import { useContext } from "react";
+
+//作成したContextをimport
+import { AdminFlagContext } from "./providers/AdminFlagProvider";
+
 const style = {
   width: "100px",
   padding: "6px",
@@ -6,6 +12,10 @@ const style = {
 
 export const EditButton = (props) => {
   const { isAdmin } = props;
+
+  //useContextの引数に参照するContextを指定する
+  const contextValue = useContext(AdminFlagContext);
+  console.log(contextValue);
 
   return (
     <button style={style} disabled={!isAdmin}>
